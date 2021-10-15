41980a513a02e7c5815de4bfa0aac3df3fa80cc6
AviralCoder <aviral.kochgaway@gmail.com>
2021-10-15 17:49:48 +0530

    remove useless function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -12,18 +12,4 @@ const convertEncodingToText = (text: string) => {
     return editedText5;
 };
 
-const shuffleArray = (array: any[]) => {
-    let currentIndex = array.length,
-        temporaryValue,
-        randomIndex;
-    while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
-};
-
-export { randomNumber, convertEncodingToText, shuffleArray };
+export { randomNumber, convertEncodingToText };
