27cbdceeaeedab3d21e4e53c67938ec959d14bfb
AviralCoder <aviral.kochgaway@gmail.com>
2021-10-17 21:33:11 +0530

    add support for downloading score

--- src/components/KnowledgeScore.tsx
+++ src/components/KnowledgeScore.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import { colors } from "../lib/colors/colors";
 import { knowledgeScoreComponentProps } from "../types/types";
+import Button from "./Button";
+import domtoimage from "dom-to-image";
+import { SetAlertPropetiesContext, AlertPropertiesContext } from "../App";
 
 const KnowledgeScoreDiv = styled.div`
     border: 2px solid ${colors.secondary};
@@ -28,12 +31,54 @@ const Score = styled.h1`
 `;
 
 const KnowledgeScore = (props: knowledgeScoreComponentProps): JSX.Element => {
+    const setAlertProperties = useContext(SetAlertPropetiesContext);
+    const alertProperties = useContext(AlertPropertiesContext);
+    const [buttonText, setButtonText] = useState("Download");
+    const [headerText, setHeaderText] = useState(
+        "You have gained this much knowledge -"
+    );
+
     return (
         <React.Fragment>
-            <KnowledgeScoreDiv>
-                <Header>You have gained this much knowledge - </Header>
+            <KnowledgeScoreDiv id="knowledge_score">
+                <div>
+                    <Header>{headerText}</Header>
 
-                <Score>{props.score}</Score>
+                    <Score>{props.score}</Score>
+                </div>
+
+                <Button
+                    onClick={() => {
+                        const node = document.getElementById("knowledge_score");
+
+                        setButtonText(":)");
+                        setHeaderText(
+                            `The user has fairly won ${props.score} gyanpoints in the KBG app!`
+                        );
+
+                        domtoimage
+                            .toJpeg(node!, { quality: 0.95 })
+                            .then((dataUrl) => {
+                                const link = document.createElement("a");
+                                link.download = "score.jpeg";
+                                link.href = dataUrl;
+                                link.click();
+                                link.remove();
+
+                                setButtonText("Download");
+                                setHeaderText(
+                                    `"You have gained this much knowledge -"`
+                                );
+                            });
+
+                        setButtonText(":)");
+                        setHeaderText(
+                            `The user has fairly won ${props.score} in the KBG app!`
+                        );
+                    }}
+                >
+                    {buttonText}
+                </Button>
             </KnowledgeScoreDiv>
         </React.Fragment>
     );
