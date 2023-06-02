44a0d6cbacd8932d556693c4a00f9cb4254d9220
Aviral <82994899+AviralCoder@users.noreply.github.com>
2023-06-03 00:24:51 +0530

    Update instructions_lib.ts

--- src/lib/instructions_lib.ts
+++ src/lib/instructions_lib.ts
@@ -1,17 +1,6 @@
 export const body = `
-Welcome the KBG - Kaun Banega Gyanpati app! In this
-app you will get a random question on the screen. If you
-get it correct, your knowledge score will keep on
-increasing but if you get it wrong, the game will stop. But never ever take a chance,
-either quit or we have got 2 lifelines for you, Flip The Question lifeline - it
-helps you change the question (there are 2 of these).  Remember this, you have 
-only one chance to click the questions. If you click on any 
-option then we will take it to be the one chosen by you, so don't
-misclick! You have 60 seconds to answer each question.
-If you change tabs, then we will consider it as
-cheating and stop the game. You may download your score as an image if you want to.
-Your difficulty will be easy at the starting of the game and will change to medium 
-at 500, hard at 1500 and you win the game at 5000.  Since
-this app is just a web app and has limitations, we recommend someone to physically monitor how
-you play.
+We are sorry to tell you that this project has ended due to 
+newer versions of the underlying systems being released.
+We are working on a new version. Please stay connected to know
+more and the link of the new website shall be posted soon!
 `;
